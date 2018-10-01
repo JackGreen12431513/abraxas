5e9ffa8f0a8e712944d47c39b2a84914cc934dbb
Jacon <preazy.rblx@gmail.com>
2018-10-01 19:38:28 +0100

    Fixed Client Ping

--- index.js
+++ index.js
@@ -148,7 +148,7 @@ var server = http.createServer();
 server.listen(process.env.PORT || 5000)
 
 setInterval(function() {
-    http.get("http://dinobotdisc.herokuapp.com");
+    http.get("http://abraxasdiscordbot.herokuapp.com");
     console.log("Pinged!")
 }, 300000); // every 5 minutes (300000)
 
